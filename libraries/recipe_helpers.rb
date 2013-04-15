6c1a95a69f04e2687ac60f021a7a2ab0f3f5ed86
Holger Just <hjust@meine-er.de>
2013-04-15 15:38:57 +0200

    Move ICMP rules into the icmp protocol section

---
+++ libraries/recipe_helpers.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: iptables-persistent
+#
+# Copyright 2013, Planio GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module IptablesPersistent
+  module RecipeHelpers
+    def prepend_rules(protocol, rule_type, rules)
+      existing = node.default["iptables-persistent"][protocol][rule_type]
+      node.default["iptables-persistent"][protocol][rule_type] = rules + existing.to_a
+    end
+  end
+end
--- recipes/secure_default.rb
+++ recipes/secure_default.rb
@@ -19,75 +19,65 @@
 
 include_recipe "iptables-persistent"
 
+extend IptablesPersistent::RecipeHelpers
+
 node.default["iptables-persistent"]["ipv4"]["defaults"]["INPUT"] = "DROP"
 node.default["iptables-persistent"]["ipv4"]["defaults"]["OUTPUT"] = "ACCEPT"
 node.default["iptables-persistent"]["ipv4"]["defaults"]["FORWARD"] = "DROP"
 
-node.default["iptables-persistent"]["ipv4"]["any_pre"] = [
-  "\n# allow any traffic over loopback",
+prepend_rules "ipv4", "any_pre", [
+  "# allow any traffic over loopback",
   {"chain" => "INPUT", "interface" => "lo"},
   {"chain" => "OUTPUT", "interface" => "lo"},
 
-  "\n# reject any traffic from or to 127/0 that doesn't involve the loopback interface",
-  {"!interface" => "lo", "destination" => "127.0.0.1/8", "target" => "REJECT"},
+  {"!interface" => "lo", "destination" => "127.0.0.1/8", "target" => "REJECT", "comment" => "Reject any traffic from or to 127/0 that doesn't involve the loopback interface."},
+  {"protocol" => "tcp", "state" => "NEW", "opts" => ["! --syn"], "target" => "DROP", "comment" => "Drop any TCP packet that does not start a connection with a syn flag."},
 
-  "\n# Drop any TCP packet that does not start a connection with a syn flag.",
-  {"protocol" => "tcp", "state" => "NEW", "opts" => ["! --syn"], "target" => "DROP"},
-
-  "\n# allow established or related traffic to any chain",
-  {"chain" => "INPUT", "state" => %w[ESTABLISHED RELATED]},
+  {"chain" => "INPUT", "state" => %w[ESTABLISHED RELATED], "comment" => "Allow established or related traffic to any chain."},
   {"chain" => "OUTPUT", "state" => %w[ESTABLISHED RELATED]},
   {"chain" => "FORWARD", "state" => %w[ESTABLISHED RELATED]},
 
-  "\n# Drop any incomming invalid packet that could not be identified.",
-  {"state" => "INVALID", "target" => "DROP"},
+  {"state" => "INVALID", "target" => "DROP", "comment" => "Drop any incomming invalid packet that could not be identified."},
+]
 
-  "\n# ICMP echo-request",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 0"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 0"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 0"]},
+prepend_rules "ipv4", "icmp", [
+  {"chain" => "INPUT", "opts" => ["--icmp-type 0"], "comment" => "ICMP echo-request"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 0"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 0"]},
 
-  "\n# ICMP host-unreachable",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 3/1"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 3/1"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 3/1"]},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 3/1"], "comment" => "ICMP host-unreachable"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 3/1"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 3/1"]},
 
-  "\n# ICMP port-unreachable",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 3/3"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 3/3"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 3/3"]},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 3/3"], "comment" => "ICMP port-unreachable"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 3/3"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 3/3"]},
 
-  "\n# ICMP fragmentation-needed (filtering that is a BAD idea)",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 3/4"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 3/4"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 3/4"]},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 3/4"], "comment" => "ICMP fragmentation-needed (filtering that is a BAD idea)"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 3/4"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 3/4"]},
 
-  "\n# ICMP source quench RFC 792 (filtering that is a BAD idea)",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 4"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 4"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 4"]},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 4"], "comment" => "ICMP source quench RFC 792 (filtering that is a BAD idea)"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 4"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 4"]},
 
-  "\n# ICMP echo-reply, limited to 2 per second",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 8", "-m limit", "--limit 2/s"]},
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 8"], "target" => "DROP"},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 8"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 8"]},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 8", "-m limit", "--limit 2/s"], "comment" => "ICMP echo-reply, limited to 2 per second"},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 8"], "target" => "DROP"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 8"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 8"]},
 
-  "\n# ICMP Time Exceeded",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 11"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 11"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 11"]},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 11"], "comment" => "ICMP Time Exceeded"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 11"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 11"]},
 
-  "\n# ICMP Parameter Problem",
-  {"chain" => "INPUT", "protocol" => "icmp", "opts" => ["--icmp-type 12"]},
-  {"chain" => "OUTPUT", "protocol" => "icmp", "opts" => ["--icmp-type 12"]},
-  {"chain" => "FORWARD", "protocol" => "icmp", "opts" => ["--icmp-type 12"]}
-] + node.default["iptables-persistent"]["ipv4"]["any_pre"]
+  {"chain" => "INPUT", "opts" => ["--icmp-type 12"], "comment" => "ICMP Parameter Problem"},
+  {"chain" => "OUTPUT", "opts" => ["--icmp-type 12"]},
+  {"chain" => "FORWARD", "opts" => ["--icmp-type 12"]}
+]
 
-node.default["iptables-persistent"]["ipv4"]["tcp"] = [
-  # Allow at least all incomming traffic on Port 22 (SSH)
-  {"port" => 22}
-] + node.default["iptables-persistent"]["ipv4"]["tcp"]
+prepend_rules "ipv4", "tcp", [
+  {"port" => 22, "comment" => "Allways allow at least Port 22 (SSH)"}
+]
 
 # completely disable ipv6 traffic
 node.default["iptables-persistent"]["ipv6"]["defaults"]["INPUT"] = "DROP"
