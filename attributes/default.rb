dcb03ce80daeac3da5723cc6c3e0d2cb036bac3a
Holger Just <hjust@meine-er.de>
2013-04-29 20:31:42 +0200

    Add emty default ICMP rules for simpler addition of rules

--- attributes/default.rb
+++ attributes/default.rb
@@ -33,6 +33,7 @@ default["iptables-persistent"]["ipv4"]["defaults"]["FORWARD"] = "ACCEPT"
 default["iptables-persistent"]["ipv4"]["any_pre"] = []
 default["iptables-persistent"]["ipv4"]["tcp"] = []
 default["iptables-persistent"]["ipv4"]["udp"] = []
+default["iptables-persistent"]["ipv4"]["icmp"] = []
 default["iptables-persistent"]["ipv4"]["any_post"] = []
 default["iptables-persistent"]["ipv6"]["nat"] = []
 
@@ -44,4 +45,5 @@ default["iptables-persistent"]["ipv6"]["defaults"]["FORWARD"] = "ACCEPT"
 default["iptables-persistent"]["ipv6"]["any_pre"] = []
 default["iptables-persistent"]["ipv6"]["tcp"] = []
 default["iptables-persistent"]["ipv6"]["udp"] = []
+default["iptables-persistent"]["ipv6"]["icmp"] = []
 default["iptables-persistent"]["ipv6"]["any_post"] = []
