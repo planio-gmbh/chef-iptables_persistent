6dd0cfd06f0d22f45bc9af2cef7e8f4c845e1f01
Holger Just <hjust@meine-er.de>
2016-09-22 13:12:40 +0200

    Increase ICMP echo-request limit to better play with some eager monitoring systems

--- recipes/secure_default.rb
+++ recipes/secure_default.rb
@@ -66,7 +66,7 @@ prepend_rules "ipv4", "filter", "icmp", [
   {"chain" => "OUTPUT", "opts" => ["--icmp-type 4"]},
   {"chain" => "FORWARD", "opts" => ["--icmp-type 4"]},
 
-  {"chain" => "INPUT", "opts" => ["--icmp-type 8", "-m limit", "--limit 2/s"], "comment" => "ICMP echo-request, limited to 2 per second"},
+  {"chain" => "INPUT", "opts" => ["--icmp-type 8", "-m limit", "--limit 3/s", "--limit-burst 5/s"], "comment" => "ICMP echo-request, limited to 3 per second"},
   {"chain" => "INPUT", "opts" => ["--icmp-type 8"], "target" => "DROP"},
   {"chain" => "OUTPUT", "opts" => ["--icmp-type 8"]},
   {"chain" => "FORWARD", "opts" => ["--icmp-type 8"]},
